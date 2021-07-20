1e179dd6d6c78e1876eccb3204a9246fe5aad86c
seonongs <seonongs@gmail.com>
2021-07-20 14:05:51 +0900

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget{
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'First App',
+      theme: ThemeData(
+        primarySwatch: Colors.yellow
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget{
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('First App'),
+      ),
+      body: Center(
+        child: Column(
+          children: const <Widget>[
+            Text('hello'),
+            Text('hello'),
+            Text('hello')
+          ],
+        ),
+
+      ),
+    );
+  }
+}
