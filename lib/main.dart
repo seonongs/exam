1200c1b5a4acc45de35bc5fd27d7997bb65d15db
seonongs <seonongs@gmail.com>
2021-07-21 02:12:18 +0900

    .

--- lib/main.dart
+++ lib/main.dart
@@ -2,82 +2,78 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/painting.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Nurschedule',
       theme: ThemeData(primarySwatch: Colors.yellow),
-      home: MyHomePage(),
+      home: const MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatelessWidget {
   const MyHomePage({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.yellow,
       appBar: AppBar(
-        title: Row(
-          children: const [
-            Text('Nurschedule', style: TextStyle(
-                color: Color.fromRGBO(0, 0, 0, 100)
-            ),),
-          ],
+        title: const Text(
+          'Nurschedule',
+          style: TextStyle(color: Color.fromRGBO(0, 0, 0, 100)),
         ),
-        backgroundColor: Colors.yellowAccent,
         elevation: 0.0,
-        actions: [
+        actions: <Widget>[
           IconButton(
-              icon: Icon(Icons.settings, color: Color.fromRGBO(0, 0, 0, 10),),
+              icon: const Icon(
+                Icons.settings,
+                color: Color.fromRGBO(0, 0, 0, 10),
+              ),
               onPressed: () {
                 print('setting');
-              }
-          ),
+              }),
           IconButton(
-            icon: Icon(Icons.logout, color: Color.fromRGBO(0, 0, 0, 11),),
+            icon: const Icon(
+              Icons.logout,
+              color: Color.fromRGBO(0, 0, 0, 11),
+            ),
             onPressed: () {
               print('logout');
             },
           ),
         ],
       ),
-      body: Center(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: const <Widget>[
-            Text(
-              'data',
-              style: TextStyle(
-                color: Colors.redAccent,
-                letterSpacing: 12.0,
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            UserAccountsDrawerHeader(
+              currentAccountPicture: const CircleAvatar(
+                backgroundImage: AssetImage('assets/ssk1.png'),
+                backgroundColor: Colors.white,
               ),
-            ),
-            SizedBox(
-              height: 10.0,
-            ),
-            Text(
-              'data',
-              style: TextStyle(
-                  color: Colors.blueAccent,
-                  letterSpacing: 2.0,
-                  fontSize: 30,
-                  fontWeight: FontWeight.bold),
-            ),
-            Text(
-              'data',
-              style: TextStyle(
-                color: Color.fromRGBO(0, 0, 0, 100),
-                backgroundColor: Color.fromRGBO(255, 255, 255, 100),
-                fontSize: 50,
-                letterSpacing: 1.5,
-              ),
-            ),
+              accountName: const Text('신세경', style: TextStyle(
+                color: Color.fromRGBO(10, 0, 0, 100)
+              ),),
+              accountEmail: const Text('ssk@ssk.com', style: TextStyle(
+                  color: Color.fromRGBO(0, 0, 0, 200)
+              ),),
+              onDetailsPressed: () {
+                print('arrow is clicked');
+              },
+              decoration: const BoxDecoration(
+                  color: Color.fromRGBO(220, 20, 20, 100),
+                  borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(40.0),
+                    bottomRight: Radius.circular(40.0),
+                  )),
+            )
           ],
         ),
       ),
